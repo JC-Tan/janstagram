55cc6fa91b937edbae50018f4c4ec6aff57bd97a
JC-Tan <93283233+JC-Tan@users.noreply.github.com>
2023-06-21 17:36:02 -0500

    feat(ClickPost): Posts are clickable

--- app/components/Ui/Modal/Modal.tsx
+++ app/components/Ui/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import Flex from '../Flex/Flex'
 import PackageModal from 'react-modal'
 
@@ -6,13 +6,20 @@ PackageModal.setAppElement('#layout')
 
 interface IModal {
   isOpen: boolean
+  onClose?: () => void
   children?: ReactNode
 }
 
-const Modal = ({ isOpen, children }: IModal) => {
+const Modal = ({ isOpen, children, onClose }: IModal) => {
   return (
     <Flex>
-      <PackageModal isOpen={isOpen}>
+      <PackageModal
+        isOpen={isOpen}
+        shouldCloseOnOverlayClick={true}
+        shouldCloseOnEsc={true}
+        preventScroll={true}
+        onRequestClose={onClose}
+      >
         <Flex justifyContent='center' alignItems='center'>
           {children}
         </Flex>
